ebb994cc9c2f5c142b46c1710a1be89fa701aa34
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 10:45:43 -0500

    tunit

--- test/Soenneker.Loops.OpenApiClient.Tests/LoopsOpenApiClientTests.cs
+++ test/Soenneker.Loops.OpenApiClient.Tests/LoopsOpenApiClientTests.cs
@@ -1,16 +1,15 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Loops.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public class LoopsOpenApiClientTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class LoopsOpenApiClientTests : HostedUnitTest
 {
-    public LoopsOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public LoopsOpenApiClientTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
